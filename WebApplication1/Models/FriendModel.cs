4b69b0cd40419a9141456f65c1b4c8a0ffcff285
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2020-10-19 18:51:45 +0500

    First

---
+++ WebApplication1/Models/FriendModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FriendModel
+    {
+        public string FriendName { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+
+    }
+}
